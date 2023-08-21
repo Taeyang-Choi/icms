5de69a6586eaf74fc7f57b5efbf54d420d47c47c
choetaeyang <zhfldk1027@gmail.com>
2023-08-22 01:52:42 +0900

    76번 공지사항 댓글 표시기능 추가

--- src/main/java/com/ogp/icms/board/controller/CommentApiController.java
+++ src/main/java/com/ogp/icms/board/controller/CommentApiController.java
@@ -1,6 +1,7 @@
 package com.ogp.icms.board.controller;
 
 import com.ogp.icms.board.domain.Comment;
+import com.ogp.icms.board.domain.Comments;
 import com.ogp.icms.board.service.CommentServiceImpl;
 import com.ogp.icms.global.util.ResultCode;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +29,17 @@ public class CommentApiController {
     @GetMapping("/api/comment/list/{id}")
     public List<Comment> getList(@PathVariable Long id) throws IOException {
         return commentService.getList(id);
+    }
+
+    @GetMapping("/api/comment/lists/{ids}")
+    public List<Comments> getList(@PathVariable String ids) throws IOException {
+        System.out.println(ids +" "+ids.replaceAll("_", ","));
+        String[] _ids = ids.split("_");
+        List<String> __ids = new ArrayList<>();
+        for (int i=0; i<_ids.length; i++) {
+            __ids.add(_ids[i]);
+        }
+        return commentService.getList(__ids);
     }
 
     @GetMapping("/api/comment/under/{pos}")
